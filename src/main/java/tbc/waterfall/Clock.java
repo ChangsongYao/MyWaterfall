c593b2424f86fe434fb79620aba7769c005a7b5f
Hidetada-komatu <2543144515@qq.com>
2017-12-30 19:26:47 +0800

    增加makeHourWithin0To23(int hour)方法； 将时间对24取余数 (hour+24)%24即可。

--- src/main/java/tbc/waterfall/Clock.java
+++ src/main/java/tbc/waterfall/Clock.java
@@ -4,16 +4,20 @@ public class Clock {
     protected int UTC_OFFSET = 0;
     protected int localTime = 0;
 
-    public Clock(int utcOffset){
+    public Clock(int utcOffset) {
         this.UTC_OFFSET = utcOffset;
     }
 
-    public void setLocalTime(int localTime){
+    public void setLocalTime(int localTime) {
         this.localTime = localTime;
     }
 
-    public void setLocalTimeFromUtcZeroTime(int utcZeroTime){
-        this.localTime = utcZeroTime + this.UTC_OFFSET;
+    public void setLocalTimeFromUtcZeroTime(int utcZeroTime) {
+        this.localTime = Clock.makeHourWithin0To23(utcZeroTime + this.UTC_OFFSET);
+    }
+
+    private static int makeHourWithin0To23(int hour) {
+        return (hour + 24) % 24;
     }
 
     public String getTime() {
